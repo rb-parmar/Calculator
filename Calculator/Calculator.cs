2452aae02a063a85b75fcab24f48a713ec2053b7
Rushit <rushitparmar59@gmail.com>
2023-09-30 22:52:24 -0500

    Added appropriate buttons and labels Gave names and styling

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
